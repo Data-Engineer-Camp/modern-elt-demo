65d12b415ea69a159bde17a807b385c45b72d4f1
Jonathan Neo <jonathanneo@me.com>
2022-09-02 02:06:24 +0800

    airbyte

--- orchestrate/elt/elt/repository.py
+++ orchestrate/elt/elt/repository.py
@@ -1,8 +1,55 @@
-from dagster import load_assets_from_package_module, repository
+from dagster import (
+    ScheduleDefinition,
+    repository, 
+    job
+)
 
-from elt import assets
+from dagster_airbyte import airbyte_resource, airbyte_sync_op
+from dagster_dbt import dbt_run_op, dbt_cli_resource
+from .utils.constants import AIRBYTE_CONNECTION_ID, AIRBYTE_CONFIG, DBT_CONFIG
 
+sync_dvd_rental = airbyte_sync_op.configured({"connection_id": AIRBYTE_CONNECTION_ID}, name="sync_dvd_rental")
+
+@job(resource_defs={
+        "airbyte": airbyte_resource.configured(AIRBYTE_CONFIG),
+        "dbt": dbt_cli_resource.configured(DBT_CONFIG)
+    })
+def elt_job():
+    dbt_run_op(sync_dvd_rental())
 
 @repository
 def elt():
-    return [load_assets_from_package_module(assets)]
+    return [
+        # update all assets once a day
+        ScheduleDefinition(
+            job=elt_job, cron_schedule="@daily"
+        )
+    ]
+
+# from dagster_airbyte import airbyte_resource
+# from dagster_dbt import dbt_cli_resource
+
+# from dagster import (
+#     ScheduleDefinition,
+#     define_asset_job,
+#     load_assets_from_package_module,
+#     repository,
+#     with_resources,
+# )
+
+# from . import assets
+# from .utils.constants import AIRBYTE_CONFIG, DBT_CONFIG
+
+# from dagster import graph, job, op, get_dagster_logger
+# from dagster_airbyte import airbyte_resource, airbyte_sync_op
+# from dagster_dbt import dbt_run_op, dbt_cli_resource
+
+# @job(resource_defs={
+#         # "airbyte": airbyte_resource,
+#         "dbt": dbt_cli_resource.configured(DBT_CONFIG)
+#     })
+# def elt_job():
+#     # sync_dvd_rental()
+#     dbt_run_op()
+
+
--- orchestrate/elt/setup.py
+++ orchestrate/elt/setup.py
@@ -4,8 +4,12 @@ if __name__ == "__main__":
     setup(
         name="elt",
         packages=find_packages(exclude=["elt_tests"]),
+        package_data={"elt": ["dw/*"]},
         install_requires=[
             "dagster",
+            "dagster-airbyte",
+            "dagster-dbt",
+            "dbt-core",
         ],
         extras_require={"dev": ["dagit", "pytest"]},
     )
