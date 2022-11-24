7c5ada4fcab418038ac7175fea92d61747ffb8a7
Jonathan Neo <jonathanneo@me.com>
2022-11-24 15:56:45 +0800

    dim store

--- transform/dw/models/serving/dim_store.sql
+++ transform/dw/models/serving/dim_store.sql
@@ -1,30 +1,47 @@
-select   
-    s.store_id,
-    manager_staff_id,
-    s.address_id as store_address_id,
-    a.address as store_address,
-    a.address2 as store_address2,
-    a.district as store_district,
-    a.city as store_city,
-    a.country as store_country,
-    a.postal_code as store_postal_code,
-    staff_id,
-    first_name as staff_first_name,
-    last_name as staff_last_name,
-    sa.address_id as staff_address_id,
-    email as staff_email,
-    active as staff_active,
-    username as staff_username,
-    sa.address as staff_address,
-    sa.district as staff_district,
-    sa.city as staff_city,
-    sa.country as staff_country,
-    sa.phone as staff_phone
-from
-  {{ ref('stg_store') }} s
-left join {{ ref('prep_address') }} a 
-  on a.address_id = s.address_id
-left join {{ ref('stg_staff') }} st
-  on st.staff_id = s.manager_staff_id
-left join {{ ref('prep_address') }} sa
-  on sa.address_id = st.address_id
+with store as ( 
+  select   
+      s.store_id,
+      a.address as store_address,
+      a.district as store_district,
+      a.city as store_city,
+      a.country as store_country,
+      a.postal_code as store_postal_code,
+      first_name as staff_first_name,
+      last_name as staff_last_name,
+      email as staff_email,
+      active as staff_active,
+      username as staff_username,
+      sa.address as staff_address,
+      sa.district as staff_district,
+      sa.city as staff_city,
+      sa.country as staff_country,
+      sa.phone as staff_phone
+  from
+    {{ ref('stg_store') }} s
+  left join {{ ref('prep_address') }} a 
+    on a.address_id = s.address_id
+  left join {{ ref('stg_staff') }} st
+    on st.staff_id = s.manager_staff_id
+  left join {{ ref('prep_address') }} sa
+    on sa.address_id = st.address_id
+)
+
+select 
+  {{ dbt_utils.surrogate_key(['store_id']) }} as store_key, 
+  store_address,
+  store_district,
+  store_city,
+  store_country,
+  store_postal_code,
+  staff_first_name,
+  staff_last_name,
+  staff_email,
+  staff_active,
+  staff_username,
+  staff_address,
+  staff_district,
+  staff_city,
+  staff_country,
+  staff_phone
+from 
+  store 
