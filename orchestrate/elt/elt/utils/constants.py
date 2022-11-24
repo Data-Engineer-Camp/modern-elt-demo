fe236cf4f6a441915feb1b525766e7c62b1526d5
Jonathan Neo <jonathanneo@me.com>
2022-11-24 14:44:27 +0800

    remove config folder

--- orchestrate/elt/elt/utils/constants.py
+++ orchestrate/elt/elt/utils/constants.py
@@ -1,7 +1,7 @@
 from dagster._utils import file_relative_path
 
-AIRBYTE_CONNECTION_ID = "3f5c1a1c-6cd8-48da-8770-542bb6b80040"
+AIRBYTE_CONNECTION_ID = "935e42bc-7851-4a10-856b-6eed632eb8be"
 AIRBYTE_CONFIG = {"host": "localhost", "port": "8000"}
 DBT_PROJECT_DIR = file_relative_path(__file__, "../../../../transform/dw")
-DBT_PROFILES_DIR = file_relative_path(__file__, "../../../../transform/dw/config")
+DBT_PROFILES_DIR = file_relative_path(__file__, "../../../../transform/dw")
 DBT_CONFIG = {"project_dir": DBT_PROJECT_DIR, "profiles_dir": DBT_PROFILES_DIR}
